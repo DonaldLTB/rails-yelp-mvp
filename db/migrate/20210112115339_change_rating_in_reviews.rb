8e9abb3a75b58e2517e7fea5d71178c4ca971da6
Lawrence Kroll <larry43.lk@gmail.com>
2021-01-12 21:48:02 +0900

    yelp finito

---
+++ db/migrate/20210112115339_change_rating_in_reviews.rb
@@ -0,0 +1,5 @@
+class ChangeRatingInReviews < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reviews, :rating, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clean Database"
+Restaurant.destroy_all
+puts "Database cleaned
+"
+puts "Creating Restaurants"
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Restaurant #{restaurant.id} is created"
+end
